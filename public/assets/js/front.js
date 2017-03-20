9ab9760275c6665c64cfcd9a6b1efa5438c725a2
Ksenia D <Ksenia D>
2017-03-19 21:31:28 -0400

    sliders added, login on top, need to revise style

--- public/assets/js/front.js
+++ public/assets/js/front.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
      console.log("hello");
-	 $('.submit').click(function(event){
-		event.preventDefault();
+	 $('#second').hide();
+	 $('#third').hide();
 	 
- 	});
-})
+	$('.submit').click(function(event){
+		event.preventDefault();
+		 $('#second').show(function() {
+			 $("#second").animate({right: '0vw'}, 1000);
+			 $("#first").animate({right: '85vw'}, 1000, function(){
+				 $('#first').hide();
+			 });
+		 });
+	 });
+	$('.submit2').click(function(event){
+		event.preventDefault();
+		 $('#third').show(function() {
+			 $("#third").animate({left: '0vw'}, 1000);
+			 $("#first").animate({left: '85vw'}, 1000, function(){
+				 $('#first').hide();
+			 });
+		 });
+	 });
+});
+
